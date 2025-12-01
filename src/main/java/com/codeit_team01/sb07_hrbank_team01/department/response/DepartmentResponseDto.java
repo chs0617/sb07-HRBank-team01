8d8ff77d0d8d1186373c4385163d9e4107560aa3
sinjawon <qlqhroal@naver.com>
2025-12-02 00:08:12 +0900

    Add  `DepartmentResponseDto`
    -  DepartmentResponseDto 커버 추가

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/department/response/DepartmentResponseDto.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/department/response/DepartmentResponseDto.java
@@ -1,5 +1,7 @@
 package com.codeit_team01.sb07_hrbank_team01.department.response;
 
+import com.codeit_team01.sb07_hrbank_team01.department.entity.Department;
+
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.UUID;
@@ -10,6 +12,15 @@ public record DepartmentResponseDto(
         String description,
         Instant establishDate,
         int employeeCount
-
 ) {
+    public  static DepartmentResponseDto from(Department department, int employeeCount){
+        return new DepartmentResponseDto(
+                department.getId(),
+                department.getName(),
+                department.getDescription(),
+                department.getEstablishDate(),
+                employeeCount
+        );
+    }
+
 }
