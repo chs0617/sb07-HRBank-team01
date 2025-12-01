44779687c2ecc004f47e0d8b175fe511a1401709
hilee <eldh2002@gmail.com>
2025-12-01 11:45:35 +0900

    test,add: PR Approve test / repository

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/empolyee/entity/Employee.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/empolyee/entity/Employee.java
@@ -1,4 +1,7 @@
 package com.codeit_team01.sb07_hrbank_team01.empolyee.entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Employee {
 }
---
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/empolyee/repository/EmployeeRepository.java
@@ -0,0 +1,4 @@
+package com.codeit_team01.sb07_hrbank_team01.empolyee.repository;
+
+public class EmployeeRepository {
+}
