d29fed314322e5ae504f23ddb57ccd40550128cf
sinjawon <qlqhroal@naver.com>
2025-12-01 14:37:00 +0900

    Refactor `Department` entity
    -어노테이션추가
    get,NoArgs,AllArgs,entity,table
    
    - establishDate : string -> LocalDate변경
    
    -update 메서드 추가

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/department/entity/Department.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/department/entity/Department.java
@@ -1,13 +1,45 @@
 package com.codeit_team01.sb07_hrbank_team01.department.entity;
 
+import com.codeit_team01.sb07_hrbank_team01.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.UUID;
 
-public class Department {
-        private UUID id;
-        private Instant createdAt;
-        private Instant updatedAt;
-        private String name;
-        private String description;
-        private Instant establishDate;
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "departments")
+public class Department extends BaseEntity {
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 200)
+    private String description;
+
+    @Column(name = "establish_date", nullable = false, columnDefinition = "DATE")
+    private LocalDate establishDate;
+
+
+    public void update(String newName, String newDescription, LocalDate newEstablishDate){
+         if(newName != null && !newName.equals(this.name)){
+             this.name = newName;
+         }
+         if(newDescription != null && !newDescription.equals(this.description)){
+             this.description = newDescription;
+         }
+         if(newEstablishDate != null && !newEstablishDate.equals(this.establishDate)){
+             this.establishDate = newEstablishDate;
+         }
+
+
+    }
 }
