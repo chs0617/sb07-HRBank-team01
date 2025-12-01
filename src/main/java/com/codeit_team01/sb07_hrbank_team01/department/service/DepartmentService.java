26a0c4677be827c9915281fef54787e4b6173430
sinjawon <qlqhroal@naver.com>
2025-12-01 15:02:11 +0900

    Refactor `DepartmentService`
    -DepartmentService 인터페이스로 전환
    -basic CRUD 추가

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/department/service/DepartmentService.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/department/service/DepartmentService.java
@@ -1,4 +1,13 @@
 package com.codeit_team01.sb07_hrbank_team01.department.service;
 
-public class DepartmentService {
+import java.util.UUID;
+
+public interface DepartmentService {
+
+    DepartmentDto createDepartment(DepartmentCreateRequest request);
+    DepartmentDto updateDepartment(UUID departmentId, DepartmentUpdateRequest request);
+    void deleteDepartment(UUID departmentId);
+    DepartmentDto getDepartment(UUID departmentId);
+    Slice<DepartmentDto> searchDepartment(DepartmentSearchCondition condition);
+
 }
