8bed8e22ccf1c2127392e8ec57604b5336b73605
yumi Kim <cdcd0214@gmail.com>
2025-12-01 15:41:22 +0900

    Style : HistoryRepository UUID -> Long으로 변경

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/history/repository/HistoryRepository.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/history/repository/HistoryRepository.java
@@ -2,9 +2,6 @@ package com.codeit_team01.sb07_hrbank_team01.history.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.UUID;
-
-public interface HistoryRepository extends JpaRepository<HistoryRepository, UUID> {
+public interface HistoryRepository extends JpaRepository<HistoryRepository, Long> {
 
 }
