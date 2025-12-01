2c93050a18c710742563ddb488abd387669b3a33
yumi Kim <cdcd0214@gmail.com>
2025-12-01 20:53:26 +0900

    Feat : HistoryRepository 작성 (불필요한 메서드 삭제)

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/history/repository/HistoryRepository.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/history/repository/HistoryRepository.java
@@ -5,13 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
 
-    // 이력 등록
-    public void createHistory(History history);
-
-    // 이력 목록 조회
-    public void findAllByHistory(History history);
-
-    // 이력 상세 변경 내용 조회
-    public History findHistoryById(Long id);
-
 }
