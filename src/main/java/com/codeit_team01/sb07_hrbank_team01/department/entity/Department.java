8b574e0a9f734584c33f0898734c84e1084fb5b0
sinjawon <qlqhroal@naver.com>
2025-12-01 13:49:01 +0900

    Feat essential fields to `Department`
    -Department필드추가

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/department/entity/Department.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/department/entity/Department.java
@@ -1,4 +1,13 @@
 package com.codeit_team01.sb07_hrbank_team01.department.entity;
 
+import java.time.Instant;
+import java.util.UUID;
+
 public class Department {
+        private UUID id;
+        private Instant createdAt;
+        private Instant updatedAt;
+        private String name;
+        private String description;
+        private Instant establishDate;
 }
