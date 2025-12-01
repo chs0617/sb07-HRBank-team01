abfe6434e23767a2910223a9d0e372710bd7b296
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-12-01 15:31:03 +0900

    Feat & Refactor: BaseEntity id필드 수정, Backup 도메인 작성 - BaseEntity 필드에 SWagger api 스펙에 따라 id 필드를 UUID에서 Long으로 수정 - Backup 도메인 관련 엔티티, Repository 작성

---
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/backup/repository/BackupRepository.java
@@ -0,0 +1,8 @@
+package com.codeit_team01.sb07_hrbank_team01.backup.repository;
+
+import com.codeit_team01.sb07_hrbank_team01.backup.entity.Backup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BackupRepository extends JpaRepository<Backup, Long> {
+
+}
