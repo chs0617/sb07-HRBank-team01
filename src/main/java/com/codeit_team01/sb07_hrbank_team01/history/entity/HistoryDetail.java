75de0e1232a246055ada761a6cd37c31ff78241a
yumi Kim <cdcd0214@gmail.com>
2025-12-01 15:12:41 +0900

    Feat : Employee History Employee History Entity, Employee History Detail, ENUM HistoryType, INTERFACE HistoryRepository

---
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/history/entity/HistoryDetail.java
@@ -0,0 +1,35 @@
+package com.codeit_team01.sb07_hrbank_team01.history.entity;
+
+import com.codeit_team01.sb07_hrbank_team01.base.BaseEntity;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "history_details")
+public class HistoryDetail extends BaseEntity {
+
+    // uuid
+
+    // 변경 상세 내용
+    @Column(name = "property_name", length = 100, nullable = false)
+    private String propertyName;
+
+    // 변경 전
+    @Lob
+    @Column(name = "before_value")
+   private String beforeValue;
+
+    // 변경 후
+    @Lob
+    @Column(name = "after_value")
+    private String afterValue;
+
+    // 생성 시간
+
+    // history_id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "history_id", nullable = false)
+    private History historyId;
+}
