709a26528a0a4f8e632f5e159748f0b7dfa468df
CHS <chschs0617@gmail.com>
2025-12-02 21:03:45 +0900

    feat: EmployeeRepository에 streamAll 쿼리 및 부서 카운트, 이메일 중복 체크 메서드 추가

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/employee/repository/EmployeeRepository.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/employee/repository/EmployeeRepository.java
@@ -2,8 +2,12 @@ package com.codeit_team01.sb07_hrbank_team01.employee.repository;
 
 import com.codeit_team01.sb07_hrbank_team01.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.stream.Stream;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     long countByDepartmentId(Long departmentId);
     boolean existsByEmailIgnoreCase(String email);
+    @Query("SELECT e FROM Employee e")
+    Stream<Employee> streamAll();
 }
