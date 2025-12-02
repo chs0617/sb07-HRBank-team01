6d2f52d62b209fa33ff9bab87acfa672c8f0ad05
sinjawon <qlqhroal@naver.com>
2025-12-02 15:11:28 +0900

    Refactor `Department` entity and related DTOs to use `LocalDate` for
    dto에 추가된 필드에서
    -establishDate -> establishedDate  바꿈
    - establishedDate의 타입을  LocalDate로 바꿈

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/department/entity/Department.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/department/entity/Department.java
@@ -1,10 +1,10 @@
 package com.codeit_team01.sb07_hrbank_team01.department.entity;
 
 import com.codeit_team01.sb07_hrbank_team01.base.BaseEntity;
-import com.codeit_team01.sb07_hrbank_team01.empolyee.entity.Employee;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,9 +13,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -30,28 +28,31 @@ public class Department extends BaseEntity {
     @Column(name = "description", nullable = false, length = 200)
     private String description;
 
-    @Column(name = "establish_date", nullable = false)
-    private Instant establishDate;
+    @Column(name = "established_date", nullable = false)
+    private LocalDate establishedDate;
 
 
 
-    public static Department of(String name, String description, Instant establishDate) {
+
+    public static Department of(String name, String description, LocalDate establishDate) {
         return new Department(name, description, establishDate);
     }
 
 
-
-    public void update(String newName, String newDescription, Instant newEstablishDate){
+    public void update(String newName, String newDescription, LocalDate newEstablishDate){
          if(newName != null && !newName.equals(this.name)){
              this.name = newName;
          }
          if(newDescription != null && !newDescription.equals(this.description)){
              this.description = newDescription;
          }
-         if(newEstablishDate != null && !newEstablishDate.equals(this.establishDate)){
-             this.establishDate = newEstablishDate;
+         if(newEstablishDate != null && !newEstablishDate.equals(this.establishedDate)){
+             this.establishedDate = newEstablishDate;
          }
 
 
     }
+
+
+
 }
--- src/main/java/com/codeit_team01/sb07_hrbank_team01/department/response/DepartmentResponseDto.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/department/response/DepartmentResponseDto.java
@@ -4,21 +4,24 @@ import com.codeit_team01.sb07_hrbank_team01.department.entity.Department;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.UUID;
 
 public record DepartmentResponseDto(
         Long id,
         String name,
         String description,
-        Instant establishDate,
+        LocalDate establishedDate,
         int employeeCount
 ) {
     public  static DepartmentResponseDto from(Department department, int employeeCount){
+
+
         return new DepartmentResponseDto(
                 department.getId(),
                 department.getName(),
                 department.getDescription(),
-                department.getEstablishDate(),
+                department.getEstablishedDate(),
                 employeeCount
         );
     }
