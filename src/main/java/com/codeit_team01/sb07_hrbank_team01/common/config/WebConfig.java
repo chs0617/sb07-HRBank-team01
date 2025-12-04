261399ee2bfc64ff46b51b5656feaaa54a2acf99
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-12-05 03:54:42 +0900

    Refactor: WebConfig, IpInterceptor - IpInterceptor를 더 빡세게 작성했습니다.

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/common/config/WebConfig.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/common/config/WebConfig.java
@@ -15,6 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(clientIpInterceptor)
-        .addPathPatterns("/api/backup"); // backup 경로
+        .addPathPatterns("/api/backups/**"); // backup 경로
   }
 }
