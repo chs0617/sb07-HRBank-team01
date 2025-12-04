010f1084cdca0ae4c04c782e9862cf1d348df635
hilee <eldh2002@gmail.com>
2025-12-04 14:02:35 +0900

    add: 필요한 내용 추가 커밋

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/employee/repository/EmployeeRepository.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/employee/repository/EmployeeRepository.java
@@ -4,11 +4,14 @@ import com.codeit_team01.sb07_hrbank_team01.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.stream.Stream;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, EmployeeRepositoryCustom {
     int countByDepartmentId(Long departmentId);
     boolean existsByEmailIgnoreCase(String email);
 
     @Query(value = "SELECT nextval('employee_global_number')", nativeQuery = true)
+    Stream<Employee> streamAll();
     long nextEmployeeNumber();
 
     boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
