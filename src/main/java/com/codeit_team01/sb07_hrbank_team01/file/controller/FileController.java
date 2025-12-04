2f608871a2a2da019ca42ebd059939cecdee0414
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-12-05 00:39:46 +0900

    Fix: FileController 수정

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/file/controller/FileController.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/file/controller/FileController.java
@@ -6,12 +6,12 @@ import com.codeit_team01.sb07_hrbank_team01.file.storage.FileLocalStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/files")
 @RequiredArgsConstructor
 public class FileController {
