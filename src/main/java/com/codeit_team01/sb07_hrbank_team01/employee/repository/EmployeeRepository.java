99ed2ec894aa94c2987580ce295501fe5c5bfd4b
hilee <eldh2002@gmail.com>
2025-12-04 13:15:09 +0900

    add: 정렬 및 페이지네이션 구현을 위한 Custom Repository 추가 및 상속 추가, 메서드 추가

--- src/main/java/com/codeit_team01/sb07_hrbank_team01/employee/repository/EmployeeRepository.java
+++ src/main/java/com/codeit_team01/sb07_hrbank_team01/employee/repository/EmployeeRepository.java
@@ -1,12 +1,10 @@
 package com.codeit_team01.sb07_hrbank_team01.employee.repository;
 
-import com.codeit_team01.sb07_hrbank_team01.department.entity.Department;
 import com.codeit_team01.sb07_hrbank_team01.employee.entity.Employee;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, EmployeeRepositoryCustom {
     int countByDepartmentId(Long departmentId);
     boolean existsByEmailIgnoreCase(String email);
 
